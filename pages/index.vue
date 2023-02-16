762cda5fd1f0c00ed95cc216312a4fad4171fecf
th74100 <100682190+th74100@users.noreply.github.com>
2023-02-16 13:18:40 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <main id="main">
+        <h1 class="title">{{msg}}</h1>
+    </main>
+</template>
+<script>
+export default {
+    name: 'index',
+    data() {
+        return {
+            msg: 'this is test NUXT'
+        }
+    }
+}
+</script>
+<style scoped>
+    @import "@/static/css/index.css";
+</style>
